0a5a119fbf055ea60b1407549103913f49d0a38c
Mitchell Renton <mitchrenton@gmail.com>
2015-02-04 10:53:41 +0100

    Change deploy directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -293,7 +293,7 @@ module.exports = function (grunt) {
       dist: {
         options: {
           remote: 'https://github.com/mitchrenton/mitchrenton.github.io.git',
-          branch: 'gh-pages',
+          branch: 'master',
           commit: true,
           push: true
         }
